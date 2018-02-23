390b4d31062cb280f901a155a95b86dc59a96e3e
Marc Landolt <mail@marclandolt.ch>
2018-02-23 02:33:28 +0100

    tempsave

---
+++ js/grapher.js
@@ -0,0 +1,101 @@
+//grafik parameter
+var height=500;
+var width=800;
+
+var startTime = 0;
+var endTime = 0;
+
+var arrayStart=0;
+var arrayEnd=100;
+
+
+function find(array, index, value) {
+	for (i=0; i<array.length; i++)
+		if (array[i][index] > value)
+		{	
+			return i;
+		}
+}
+
+function getMinMaxOf2DIndex (arr, idx) {
+    return {
+            min: Math.min.apply(null, arr.map(function (e) { return e[idx]})),
+            max: Math.max.apply(null, arr.map(function (e) { return e[idx]}))
+        }
+} 
+
+
+function timeToPixel(startTime, endTime, time, width)
+{
+	//alert((time - startTime)/(endTime-startTime)*width);
+	return (time - startTime)/(endTime-startTime)*width;
+}
+
+
+function moveLeft() {
+	arrayStart-=5;
+	arrayEnd-=5;
+	printGraph();
+}
+
+function moveRight() {
+	arrayStart+=5;
+	arrayEnd+=5;
+	printGraph();
+}
+
+function startup() {
+	startTime = document.getElementById("startTime").value;
+	endTime = document.getElementById("endTime").value;
+	
+	arrayStart = find(stringArray, timestamp, startTime);
+	arrayEnd = find(stringArray, timestamp, endTime);
+}
+
+
+function printGraph(startTime, endTime)
+{
+
+	var udcMax=getMinMaxOf2DIndex(stringArray, udc).max;
+	var pacMax=getMinMaxOf2DIndex(stringArray, pac).max;	//ich verwende dieses nicht, der PDC ist immer ähnlich wie der PAC, verwendet man nur einen Skalierungsfaktor braucht man nur eine Skala
+	var pdcMax=getMinMaxOf2DIndex(stringArray, pdc).max;
+	var tempMax=getMinMaxOf2DIndex(stringArray, temp).max*2;
+
+
+	var canvas = document.getElementById("myCanvas");
+	var context = canvas.getContext("2d");
+
+	context.clearRect(0, 0, canvas.width, canvas.height);
+
+	context.lineCap = 'round';
+	context.lineJoin = 'round';
+
+
+	// -------------- PAC ------------------
+	
+	context.setLineDash([]);
+	context.strokeStyle = '#000000';
+	context.lineWidth=2;
+
+	context.beginPath();
+	context.moveTo(  timeToPixel ( stringArray[arrayStart][timestamp], stringArray[arrayEnd][timestamp], stringArray[arrayStart][timestamp], width )
+	 		,height-stringArray[arrayStart][pac]);
+
+	for (i=arrayStart; i<arrayEnd; i++)
+	{	
+		context.lineTo( timeToPixel ( stringArray[arrayStart][timestamp], stringArray[arrayEnd][timestamp], stringArray[i][timestamp], width ),
+				height-(stringArray[i][pac]/pdcMax*0.9*height));
+	}
+		context.lineTo( timeToPixel ( stringArray[arrayStart][timestamp], stringArray[arrayEnd][timestamp], stringArray[arrayEnd][timestamp], width ), 
+				height-(stringArray[i][pac]/pdcMax*0.9*height));
+		context.lineTo( timeToPixel ( stringArray[arrayStart][timestamp], stringArray[arrayEnd][timestamp], stringArray[arrayEnd][timestamp], width ), 
+				height);
+		context.lineTo(0, height);
+	context.closePath();
+	context.stroke();
+	context.fillStyle = "rgba(255, 0, 0, 0.6)";
+	context.fill();
+
+
+}
+
---
+++ js/loader.js
@@ -0,0 +1,127 @@
+//global variabeln
+var nFiles=6;
+var xhr=[];
+var count=0;
+
+//spaltenbenennung fürs array <-> csv
+var timestamp=0;
+var pac=5;
+var pdc=6;
+var udc=7;
+var temp=8;
+
+
+var totalLength = 0;
+
+
+//MAIN:
+loadFiles();
+
+function addCounters()
+{
+	var pdcMax=0;	
+	
+	for (i=0; i<nFiles; i++)
+	if (xhr[i].status==200)
+	{
+		xhr[i]["count"]=xhr[i].response.split("\n").length -1 - 1;  //schneidet CSV Header ab, zweites -1 berücksichtigt dass arrayfeld bei 0 beginnt
+		//alert(xhr[i].count);
+	
+	}
+	else
+	{
+		xhr[i]["count"]=0;
+	}
+}
+
+function countTotalRows()
+{
+	for (i=0; i<nFiles; i++)
+	{
+		totalLength += xhr[i].count;
+	}
+}
+
+
+var stringArray;
+
+function createArray()
+{
+	addCounters();
+	countTotalRows();
+
+	stringArray = [totalLength];
+
+	for (i=0; i<totalLength; i++)
+	{
+		stringArray[i]=[];
+		for (j=0; j<temp+1; j++)
+			stringArray[i][j]=0;
+	}
+
+	var arrayCounter = 0;
+
+	for (i=0; i<nFiles; i++)
+	{
+		for (j=0; j<xhr[i].count; j++)
+		{
+			stringArray[arrayCounter][timestamp] = xhr[i].response.split("\n")[j+1].split(";")[timestamp] ;
+			stringArray[arrayCounter][pac] = xhr[i].response.split("\n")[j+1].split(";")[pac] ;
+			stringArray[arrayCounter][pdc] = xhr[i].response.split("\n")[j+1].split(";")[pdc] ;
+			stringArray[arrayCounter][udc] = xhr[i].response.split("\n")[j+1].split(";")[udc] ;
+			stringArray[arrayCounter][temp] = xhr[i].response.split("\n")[j+1].split(";")[temp] ;
+
+			arrayCounter+=1;
+		}
+		
+	}
+
+	stringArray.sort(function(a,b){
+		    return a[timestamp] - b[timestamp];
+	});
+
+}
+
+var myString;
+
+function printArray()
+{
+	myString = "";
+
+	var div = document.getElementById('output');
+	
+	for(i=0; i<stringArray.length; i++)
+	{
+		myString += stringArray[i][timestamp] + ": ";
+		myString += stringArray[i][pac] + " - ";
+		myString += stringArray[i][pdc] + " - ";
+		myString += stringArray[i][udc] + " - ";
+		myString += stringArray[i][temp] + "<br>";
+	}
+	div.innerHTML = myString;
+}
+
+
+function loadFiles()
+{
+	alert("loadFiles");
+	for (i=0; i<nFiles; i++)
+	{
+	
+		//Hinweis, allenfalls fetch und promise verwenden
+		xhr[i] = new XMLHttpRequest();
+		xhr[i].open('POST', i+'.txt', true);
+
+		xhr[i].onload = function () {
+			count++;
+			if (count==nFiles) {
+				createArray();
+				printArray();
+				startup();
+				printGraph();
+			}
+		}; //hier darf man keine parameter übergeben wie (xhr[i], msg) sonst funktioniert das blöde XMLHttpRequest nicht richtig 
+	xhr[i].send(null);
+	}
+}
+
