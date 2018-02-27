95fc2aa70cb8c15e4deea919a6282bdf25f436f4
Marc Landolt <mail@marclandolt.ch>
2018-02-27 18:57:58 +0100

    tempsave

--- js/grapher.js
+++ js/grapher.js
@@ -83,11 +83,24 @@ class Grapher {
 		//this.canvas = document.getElementById("myCanvas");
 		this.context = this.canvas.getContext("2d");
 		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-	
+		
+		var width=this.width*10/12;
+		var height=this.height*10/12;
+
+		var offsetX = this.width/12;
+		var offsetY = this.height/12;
+
+		this.yMax=this.getMinMaxOf2DIndex(this.loader.stringArray.slice(this.arrayStart, this.arrayEnd), pdc).max;
+
+
+		this.makeGrid(offsetX, offsetY, width, height);
+
 		this.printSubGraph(pac, "#FF0000", true, "rgba(255, 0, 0, 0.6)");
 		this.printSubGraph(pdc, "#FFFF00", true, "rgba(255, 255, 0, 0.6)");
 		this.printSubGraph(udc, "#0000FF", false, "" );
 		this.printSubGraph(temp, "#555555", false, "" );
+
+		this.makeAxis(offsetX, offsetY, width, height);
 	}
 
 	printSubGraph(value, color, solid, fill)
@@ -98,10 +111,6 @@ class Grapher {
 		var offsetX = this.width/12;
 		var offsetY = this.height/12;
 
-		this.makeAxis(offsetX, offsetY, width, height);
-
-
-		var yMax=this.getMinMaxOf2DIndex(this.loader.stringArray.slice(this.arrayStart, this.arrayEnd), value).max;
 
 		this.context.lineCap = 'round';
 		this.context.lineJoin = 'round';
@@ -112,19 +121,19 @@ class Grapher {
 
 		this.context.beginPath();
 		//this.context.moveTo( offsetX + this.timeToPixel ( this.loader.startTime, this.loader.endTime, this.loader.startTime, width ),
-		// 		 this.height - offsetY - this.loader.stringArray[this.arrayStart][value] / yMax * height );
+		// 		 this.height - offsetY - this.loader.stringArray[this.arrayStart][value] / this.yMax * height );
 	
 
 		for (var i=this.arrayStart; i<this.arrayEnd; i++)
 		{
 			this.context.lineTo( offsetX + this.timeToPixel ( this.loader.startTime, this.loader.endTime, this.loader.stringArray[i][timestamp], width ),
-					this.height - offsetY - this.loader.stringArray[i][value] / yMax * height);
+					this.height - offsetY - this.loader.stringArray[i][value] / this.yMax * height);
 		}
 
 		if (solid==true)
 		{
 		this.context.lineTo( this.timeToPixel ( this.loader.startTime, this.loader.endTime, this.loader.stringArray[this.arrayEnd-1][timestamp], width ),
-			this.height - offsetY - this.loader.stringArray[this.arrayEnd-1][value] / yMax * height);
+			this.height - offsetY - this.loader.stringArray[this.arrayEnd-1][value] / this.yMax * height);
 	
 		this.context.lineTo( this.timeToPixel ( this.loader.startTime, this.loader.endTime, this.loader.stringArray[this.arrayEnd-1][timestamp], width ),
 			this.height - offsetY );
@@ -138,6 +147,15 @@ class Grapher {
 	}
 
 
+	makeGrid(startX, startY, endX, endY)
+	{
+		for (var i=2; i<11; i++)
+		{
+			this.drawLine( this.width/12, this.height/12*i, this.width/12*11, this.height/12*i );
+		}
+	}
+
+
 	makeAxis(startX, startY, endX, endY)
 	{
 
@@ -148,12 +166,31 @@ class Grapher {
 		this.context.stroke();
 		this.context.closePath();
 
+		this.drawLegend();
+	}
+
+	drawLegend()
+	{
+		
+		for (var i=2; i<11; i++)
+		{
+			var label = (12-i)/12 * this.yMax;
+			//alert(this.yMax);
+			this.drawLabel(this.width/12, this.height/12*i, (label/1000).toFixed(2));
+		}
+	}
+	
+
+	drawLabel(x, y, value)
+	{
+		this.drawLine( x-5, y, x+5, y );
+		
 		this.context.beginPath();
 		this.context.strokeStyle = "#000000";
 		this.context.lineWidth = 0;
 		this.context.fillStyle = "#000000";
-		this.context.font = "10px Arial";
-		this.context.fillText("Hello World",10,50);
+		this.context.font = "20px Arial";
+		this.context.fillText(value,x-50,y);
 		this.context.closePath();
 	}
 
